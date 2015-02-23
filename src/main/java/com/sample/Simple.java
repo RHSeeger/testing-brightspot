fe5f93312d58cd27abe0eb00fc7cc632cee5533a
Robert Seeger <rseeger@perfectsensedigital.com>
2015-02-23 12:10:18 -0500

    Added a simple test class and cases

---
+++ src/main/java/com/sample/Simple.java
@@ -0,0 +1,10 @@
+package com.sample;
+
+/**
+ * Created by rhseeger on 2/23/15.
+ */
+public class Simple {
+    public static int max(int a, int b) {
+        return a >= b ? a : b;
+    }
+}
---
+++ src/test/java/com/sample/Simple_Test.java
@@ -0,0 +1,50 @@
+package com.sample;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by rhseeger on 2/23/15.
+ */
+public class Simple_Test {
+    @Rule public TestName testName = new TestName();
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Running tests from class [" + Simple_Test.class.getName() + "]");
+    }
+
+    @AfterClass
+    public static void afterClass() {}
+
+    @Before
+    public void before() {
+        System.out.println("Running test [" + testName + "]");
+    }
+
+    @After
+    public void after() {}
+
+    @Test
+    public void max_first() {
+        assertEquals(2, Simple.max(2,1));
+    }
+
+    @Test
+    public void max_second() {
+        assertEquals(3, Simple.max(2,3));
+    }
+
+    @Test
+    public void max_same() {
+        assertEquals(4, Simple.max(4,4));
+    }
+
+}
